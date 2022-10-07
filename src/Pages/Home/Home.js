fbf0a3d3370e6405e7564db7228f7a5cd6937868
itseskimo <ayushmatmotgill@gmail.com>
2022-10-08 00:09:44 +0530

    e

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -15,14 +15,14 @@ const Home = () => {
       <div className="home__row">
           <Products
             id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            title="The Lean Startup: How Constant Innovation Creates Radically Businesses Paperback"
             price={11.96}
             rating={5}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
           />
           <Products
             id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            title="Kenwood kMix Stand Mixer for Baking, Dough Hook and Whisk, 5 Litre Glass Bowl"
             price={239.0}
             rating={4}
             image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
