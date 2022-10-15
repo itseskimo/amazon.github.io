067624fb52c6850e2a8fe3c4782cad2e8fe7b1eb
ayush <ayushmatmotgill@gmail.com>
2022-10-15 01:56:03 -0700

    hh

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import Products from '../../Components/Products/Products';
+import axios from 'axios';
 import "./Home.css";
+import { useEffect,useState } from 'react';
+import { add } from "../../Redux/cartSlice";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+ const[data,setData]=useState([])
+  useEffect(()=>{
+  axios.get('https://fakestoreapi.com/products')
+  .then((res) =>  setData(res.data))
+  .catch((err) =>  console.log(err))
+  },[])
+  
+  const dispatch = useDispatch();
+  const handleAdd = ( id, title, image, price, rating ) => {
+    dispatch(add({ id, title, image, price, rating }));
+};
+
   return (
     <div>
     <div className="home">
@@ -12,60 +27,38 @@ const Home = () => {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
-      <div className="home__row">
-          <Products
-            id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Businesses Paperback"
-            price={11.96}
-            rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-          />
-          <Products
-            id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, 5 Litre Glass Bowl and Steel Rotators"
-            price={239.0}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
-          />
-        </div>
+</div>
 
-        <div className="home__row">
-          <Products
-            id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-            price={199.99}
-            rating={3}
-            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
-          />
-          <Products
-            id="23445930"
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-            price={98.99}
-            rating={5}
-            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
-          />
-          <Products
-            id="3254354345"
-            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-            price={598.99}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
-          />
-        </div>
+</div>
 
-        <div className="home__row">
-          <Products
-            id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
-          />
-        </div>
+<div className="container" >
+{data.map((item)=>{
+  return <div className="product" key={item.id}>
+      <div className="product__info">
+        <p>{item.title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{item.price}</strong>
+        </p>
+      <div className="product__rating">
+          {Array(item.rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+      </div>
+      </div>
+      <img src={item.image} alt="" />
+      <button onClick={()=> handleAdd( item.id, item.title, item.image, item.price, item.rating )}>Add to Basket</button>
+    </div>
+
+ })} 
+  </div>
+        
+        
       </div>
 
-    </div>
-    </div>
+    
   )
 }
 
